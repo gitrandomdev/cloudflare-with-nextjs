49637e551fec6f1f1d03e8d41d47cb7cd935d078
gitmahin <nimahin25@gmail.com>
2025-03-03 07:25:44 +0600

    updated

---
+++ src/app/docs/developer/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface DataType {
+    userId: number,
+    title: string,
+    body: string,
+    id: number
+}
+
+export default async function ServerlessPage() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+
+    const data = await response.json()
+
+    return (
+        <div>
+            {
+                data.map((item: DataType, i: number) => {
+                    return <div key={i}>
+                        <span>{item.id}</span>
+                        <h1>{item.title}</h1>
+                        <p>{item.body}</p>
+                    </div>
+                })
+            }
+        </div>
+    )
+}
+
+
